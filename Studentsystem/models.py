11a7f3f694f3ce7d198a8ef757291afd32bdca3d
TabithaAluel <alueltabitha@gmail.com>
2021-07-12 23:19:08 +0000

    django

---
+++ Studentsystem/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Studentsystem(models.Model):
+    first_name = models.CharField(
+        max_length=12
+    )
+    last_name = models.CharField(
+        max_length=12
+    )
+    age = models.PositiveSmallIntegerField()
+    date_of_birth = models.DateField()
